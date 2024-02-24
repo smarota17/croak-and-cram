b038d70ab9ad1dc956fe185edf1a6066df867922
Olivia Carson <ocarson14@yahoo.com>
2024-02-24 14:09:19 -0500

    basic application setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+
+const app = express();
+
+const PORT =3000;
+const directory = __dirname + "/templates/";
+
+app.listen(PORT, () => console.log (`listening on port: ${PORT}`));
+
+app.use(express.static('static'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req,res) =>{
+    res.sendFile(directory + 'index.html');
+})
